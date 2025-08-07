e6d4634173cc11682f034f5a813898d4a71f742c
Ashish Tiwari <ashish.tiwari@peoplestrong.com>
2025-08-07 11:29:00 +0530

    Initial commit

---
+++ MyLibrary.vb
@@ -0,0 +1,8 @@
+﻿Namespace MyLibrary
+    Public Class GreetingsHelper
+        Public Shared Function BuildGreeting(name As String, color As String) As String
+            Return $"Hello, {name}! Your favorite color ashish is {color}."
+        End Function
+    End Class
+End Namespace
+
