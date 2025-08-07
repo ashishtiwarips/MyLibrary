2cd8cd82d36bb09d05f491db75a6d394e1103b9e
Ashish Tiwari <ashish.tiwari@peoplestrong.com>
2025-08-07 19:11:31 +0530

    Library done

--- MyLibrary.vb
+++ MyLibrary.vb
@@ -1,7 +1,7 @@
 ﻿Namespace MyLibrary
     Public Class GreetingsHelper
         Public Shared Function BuildGreeting(name As String, color As String) As String
-            Return $"Hello, {name}! Your favorite color ashish is {color}."
+            Return $"Hello, {name}! Your favorite color (colour) is {color}."
         End Function
     End Class
 End Namespace
